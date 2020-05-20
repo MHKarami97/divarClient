70f78c126998eae8a3f1ac01048e8bc8d7580131
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2020-05-20 18:34:35 +0430

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
+import { SearchComponent } from './search/search.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './main/main.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { GotopComponent } from './gotop/gotop.component';
+import { FooterComponent } from './footer/footer.component';
+import { CategoryComponent } from './category/category.component';
+import { BannerComponent } from './banner/banner.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    CategoryComponent,
+    FooterComponent,
+    GotopComponent,
+    HeaderComponent,
+    HomeComponent,
+    MainComponent,
+    MenuComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
