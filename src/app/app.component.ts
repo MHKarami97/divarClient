b54faa872b815eb5ed5ff70ceae585342309f1d3
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2020-06-07 16:45:18 +0430

    mkae js load better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,30 @@ import { Title } from '@angular/platform-browser';
   template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
+
+  staticJs = [
+    'vendor/jquery-1.12.4.min.js', 'vendor/modernizr-3.6.0.min.js',
+    'ajax-contact.js', 'bootstrap.min.js',
+    'jquery-ui.min', 'jquery.magnific-popup.min.js',
+    'jquery.nice-select.min.js', 'map-script.js',
+    'menu_horizontal.js', 'slick.min.js',
+    'validator.min.js', 'main.js',
+    'popper.min.js'];
+
   constructor(private titleService: Title) {
+    for (const i of this.staticJs) {
+      this.loadScript('/assets/js/' + i);
+    }
   }
 
   public setTitle(newTitle: string) {
     this.titleService.setTitle('سایت | ' + newTitle);
+  }
+
+  public loadScript(url: string) {
+    const node = document.createElement('script');
+    node.src = url;
+    node.type = 'text/javascript';
+    document.getElementsByTagName('body')[0].appendChild(node);
   }
 }
